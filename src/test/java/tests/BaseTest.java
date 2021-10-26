ff48dab54fc5450e67e042c7cb7279689cb01eda
Valentin <vasilyev.valik@gmail.com>
2021-10-26 17:27:42 +0300

    26.10

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -31,13 +31,12 @@ import java.util.concurrent.TimeUnit;
     public void start()throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--headless");
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("headless"); // Bypass OS security model
         options.addArguments("--remote-debugging-port=9222");
         WebDriver driver = new ChromeDriver(options);
         main = PageFactory.initElements(driver, MainPage.class);
