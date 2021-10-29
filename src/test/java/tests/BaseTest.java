dd873729065e7eaea44e46080625a807e22c2506
Valentin <vasilyev.valik@gmail.com>
2021-10-29 19:46:12 +0300

    29/10

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -35,20 +35,19 @@ import java.util.concurrent.TimeUnit;
         WebDriverManager.firefoxdriver().setup();
         FirefoxOptions options = new FirefoxOptions();
         driver = new FirefoxDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
 
     //   System.setProperty("webdriver.gecko.driver", "C:\\QA\\AutoTesting\\Color-it\\geckodriver.exe"); ///usr/local/bin/geckodriver - ubuntu
-    //    FirefoxOptions options = new FirefoxOptions();
-     //   options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--no-sandbox"); // Bypass OS security model
     //    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
       // options.addArguments("--headless");
    //    options.addArguments("disable-infobars"); // disabling infobars
      //   options.addArguments("--disable-extensions"); // disabling extensions
      //   options.addArguments("--disable-gpu"); // applicable to windows os only
   //      options.addArguments("--remote-debugging-port=9222");
-   //      WebDriver driver = new FirefoxDriver(options);
-         driver.manage().window().maximize();
+   //
+        //     WebDriver driver = new FirefoxDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         main = PageFactory.initElements(driver, MainPage.class);
         productCardPage= PageFactory.initElements(driver, ProductCardPage.class);
         cartPage= PageFactory.initElements(driver, CartPage.class);
