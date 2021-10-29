d9da8bab131a5dda1de3286a784b84e99a1635d5
Valentin <vasilyev.valik@gmail.com>
2021-10-30 02:44:51 +0300

    30/10

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.*;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterMethod;
@@ -14,7 +17,8 @@ import java.net.URL;
 
 public class BaseTest {
 
-    public WebDriver driver;;
+    public WebDriver driver;
+    ;
     public MainPage main;
     public ProductCardPage productCardPage;
     public CartPage cartPage;
@@ -23,7 +27,23 @@ public class BaseTest {
 
     @BeforeMethod
 
-        public RemoteWebDriver start() throws InterruptedException, MalformedURLException {
+    public void  start() {
+
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--remote-debugging-port=9222");
+        driver = new FirefoxDriver(options);
+        main = PageFactory.initElements(driver, MainPage.class);
+        productCardPage = PageFactory.initElements(driver, ProductCardPage.class);
+        cartPage = PageFactory.initElements(driver, CartPage.class);
+        checkOutPage = PageFactory.initElements(driver, CheckOutPage.class);
+
+     }
+
+
       /*  System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 
         FirefoxOptions options = new FirefoxOptions();
@@ -31,41 +51,28 @@ public class BaseTest {
         options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--remote-debugging-port=9222");
-        WebDriver driver = new RemoteWebDriver(new URL("http://http://jenkins01.color-it.ua:4444/wd/hub"), options);
+        WebDriver driver = new RemoteWebDriver(new URL("http://http://jenkins01.color-it.ua:4444/wd/hub"), options);*/
 
-  /*          WebDriverManager.firefoxdriver().setup();
+
         //    System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
-            FirefoxOptions options = new FirefoxOptions();
 
-            options.addArguments("--no-sandbox"); // Bypass OS security model
-            options.addArguments("--headless");
+
         //    options.addArguments("disable-infobars"); // disabling infobars
         //    options.addArguments("--disable-extensions"); // disabling extensions
-      //      options.addArguments("--disable-gpu"); // applicable to windows os only
-            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            options.addArguments("--remote-debugging-port=9222");
-         //   driver = new FirefoxDriver(options);      */
-
-        URL hubUrl = new URL(node);
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "firefox");
-        capabilities.setCapability("version", "93.0");
-        capabilities.setCapability("platform", "linux");
-        return new RemoteWebDriver(hubUrl, capabilities);
+        //      options.addArguments("--disable-gpu"); // applicable to windows os only
+        // overcome limited resource problems
+        //   driver = new FirefoxDriver(options);      */
 
 
-   //     DesiredCapabilities capabilities = new DesiredCapabilities();
-       // If this cap isn't specified, it will just get any available one
- //       capabilities.setCapability("network", true); // To enable network logs
-   //     capabilities.setCapability("visual", true); // To enable step by step screenshot
-     //   capabilities.setCapability("video", true); // To enable video recording
-     //   capabilities.setCapability("console", true); // To capture console logs
-        main = PageFactory.initElements(driver, MainPage.class);
-        productCardPage= PageFactory.initElements(driver, ProductCardPage.class);
-        cartPage= PageFactory.initElements(driver, CartPage.class);
-        checkOutPage= PageFactory.initElements(driver, CheckOutPage.class);
+        //     DesiredCapabilities capabilities = new DesiredCapabilities();
+        // If this cap isn't specified, it will just get any available one
+        //       capabilities.setCapability("network", true); // To enable network logs
+        //     capabilities.setCapability("visual", true); // To enable step by step screenshot
+        //   capabilities.setCapability("video", true); // To enable video recording
+        //   capabilities.setCapability("console", true); // To capture console logs
 
-    }
+
+
 
     @AfterMethod
     public void close() {
