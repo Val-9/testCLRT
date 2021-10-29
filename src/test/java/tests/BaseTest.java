a5ed240381ae13877890d275d1a65bcc9d15c0a1
Valentin <vasilyev.valik@gmail.com>
2021-10-29 20:10:05 +0300

    29/10

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -37,10 +37,10 @@ import java.util.concurrent.TimeUnit;
         System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
         FirefoxOptions options = new FirefoxOptions();
         driver = new FirefoxDriver(options);
-        options.addArguments("--remote-debugging-port=9222");
+   //     options.addArguments("--remote-debugging-port=9222");
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("headless");
+    //    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--headless");
    //    options.addArguments("disable-infobars"); // disabling infobars
      //   options.addArguments("--disable-extensions"); // disabling extensions
      //   options.addArguments("--disable-gpu"); // applicable to windows os only
