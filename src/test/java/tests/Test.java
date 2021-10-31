7b980428d61630354b906afc18677d2acc85cc9e
Valentin <vasilyev.valik@gmail.com>
2021-10-31 03:19:18 +0200

    31.10

--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -21,8 +21,8 @@ public class Test  {
     public void TestJenkins () throws InterruptedException {
 
         //   WebDriverManager.firefoxdriver().setup();
-       System.setProperty("webdriver.gecko.driver", "C:\\QA\\AutoTesting\\Color-it\\geckodriver.exe");
-   //    System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+   //    System.setProperty("webdriver.gecko.driver", "C:\\QA\\AutoTesting\\Color-it\\geckodriver.exe");
+       System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
         FirefoxOptions options = new FirefoxOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--headless");
