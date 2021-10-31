c132da70469c3be595763bff36a5ca01d7ad8e4b
Valentin <vasilyev.valik@gmail.com>
2021-10-31 03:57:06 +0200

    31.10

--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -11,27 +11,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-public class Test  {
+public class Test extends BaseTest  {
 
     @org.testng.annotations.Test
     public void TestJenkins2 () throws InterruptedException {
-        //   WebDriverManager.firefoxdriver().setup();
-//        System.setProperty("webdriver.gecko.driver", "C:\\QA\\AutoTesting\\Color-it\\geckodriver.exe");
-        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
-        FirefoxOptions options = new FirefoxOptions();
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless");
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--remote-debugging-port=9222");
-        //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebDriver driver = new FirefoxDriver(options);
         driver.get("https://www.google.com");
         WebElement input = driver.findElement(By.name("q"));
         Assert.assertEquals(input.getAttribute("name"), "q");
         System.out.println(input.getAttribute("name"));
         System.out.println("Google TEST Success");
-        driver.get("https://color-it.ua");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span[class='style_logo__XsOQq']")));
+        main.goToMain().waitVisibility(By.cssSelector("span[class='style_logo__XsOQq']"));
         System.out.println(driver.getTitle());
         WebElement banner = driver.findElement(By.cssSelector("span[class='style_logo__XsOQq']"));
         System.out.println(banner.getAttribute("title"));
