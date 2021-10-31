8ba7f8c13da1fa00960e52dcae4b8e829b9a40c1
Valentin <vasilyev.valik@gmail.com>
2021-10-31 03:40:30 +0300

    31.10

--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -21,13 +21,13 @@ public class Test  {
     public void TestJenkins () throws InterruptedException {
 
         //   WebDriverManager.firefoxdriver().setup();
-//        System.setProperty("webdriver.gecko.driver", "C:\\QA\\AutoTesting\\Color-it\\geckodriver.exe");
-       System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+       System.setProperty("webdriver.gecko.driver", "C:\\QA\\AutoTesting\\Color-it\\geckodriver.exe");
+   //    System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
         FirefoxOptions options = new FirefoxOptions();
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--headless");
-  //      options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-    //    options.addArguments("--remote-debugging-port=9222");
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--remote-debugging-port=9222");
         //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebDriver driver = new FirefoxDriver(options);
         driver.get("https://color-it.ua");
