147c94ab55f9e6e428b0975bcc8640999c53a71a
Valentin <vasilyev.valik@gmail.com>
2021-11-18 15:33:54 +0200

    new

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,7 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -48,21 +45,10 @@ public class BaseTest {
         options.addArguments("--no-sandbox"); // Bypass OS security mode
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problem
         options.addArguments("--remote-debugging-port=9222");
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
         driver = new FirefoxDriver(options);
-        driver.manage().window().setSize(new Dimension(1600, 900));
-
-
-            driver.get("https://color-it.ua/");
-            System.out.println("Title is :" + " " + driver.getTitle());
-            System.out.println("Size is :" + " " + driver.manage().window().getSize());
-        Thread.sleep(30000);
-        WebElement head = driver.findElement(By.cssSelector("div[class ='swiper-slide']"));
-        head.isDisplayed();
-
-
-
-
-            main = PageFactory.initElements(driver, MainPage.class);
+        driver.manage().window().maximize();      //setSize(new Dimension(1600, 900));
+        main = PageFactory.initElements(driver, MainPage.class);
         productCardPage = PageFactory.initElements(driver, ProductCardPage.class);
         cartPage = PageFactory.initElements(driver, CartPage.class);
         checkOutPage = PageFactory.initElements(driver, CheckOutPage.class);
